67d330bf777bcc1c2cb9ae42c7b5001af18d18f3
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-03-05 20:12:22 -0300

    Ejercicio 6 mejorado

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,7 +1,7 @@
 let num = 1;
 let numRepeticiones = 1;
 
-while (num <= 30) {
+while (num <= 9) {
   if (numRepeticiones <= num) {
     document.write(num);
     numRepeticiones++;
@@ -11,7 +11,17 @@ while (num <= 30) {
     numRepeticiones = 1;
   }
 }
-
-
-
-
+let num2 = 10
+let numRepeticiones2 = 1
+while (num <= 30) {
+  if (numRepeticiones <= num) {
+    document.write(num);
+    numRepeticiones++;
+    numRepeticiones++;
+  } else {
+    document.write("<br>");
+    num++;
+    numRepeticiones = 1;
+    
+  }
+}
