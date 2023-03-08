ee0517aff184db897c1a6b43c9e3c8551e0a7d28
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-03-08 14:09:49 -0300

    Ejercicio 8 terminado

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,37 +1,5 @@
-/* let num = 30;
-let numRepeticiones = 1;
-
-while (num >= 10) {
-  if (numRepeticiones <= num) {
-    document.write(num);
-    numRepeticiones++;
-    numRepeticiones++;
-  } else {
-    document.write("<br>");
-    num--;
-    numRepeticiones = 1;
-  }
-}
-let num2 = 9
-let numRepeticiones2 = 1
-while (num2 <= 9) {
-  if (numRepeticiones2 <= num2) {
-    document.write(num2);
-    numRepeticiones2++;
-  } else {
-    document.write("<br>");
-    num2--;
-    numRepeticiones2 = 1;
-    
-  }
-} */
-/* for (let i = 30; i >= 1; i--) {
-    document.write(String(i).repeat(i) + "<br>");
-  } */
-  
-  
-  let num = 50;
-  let numRepeticiones = 25;
+let num = 50;
+let numRepeticiones = num/2;
 while (num >= 10) {
   document.write(String(num).repeat(numRepeticiones) + "<br>");
   num--;
---
+++ js/ejercicio8.js
@@ -0,0 +1,6 @@
+for(filas = 1; filas <= 50; filas++){
+    for(columnas = 1; columnas < filas; columnas++){
+        document.write(columnas)
+    }
+    document.write(filas +'<br>')
+}
